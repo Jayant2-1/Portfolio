c6dedb1a3f41bac3bc51ffc031d91e642e566e2a
Your Name <email@domain.com>
2026-03-24 09:25:20 +0530

    initial

---
+++ PL/src/components/nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const nav = () => {
+  return (
+    <div>
+      <section className='Top'>
+        <div className='navbar'>
+            
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default nav
